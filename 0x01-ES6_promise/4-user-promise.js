852ffb624493f581841b0068905e8d2cb54319a8
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-06-29 13:51:57 +0000

    resolve user signup

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,12 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-    if (firstName && lastName) {
-      const userObject = {
-        firstname: `${firstName}`,
-        lastName: `${lastName}`,
-      };
-      resolve(userObject);
-    }
-    reject(new Error());
-  });
+  return Promise.resolve({ firstName, lastName });
 }

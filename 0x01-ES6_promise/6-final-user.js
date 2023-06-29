78515ad8485f0cbac1925d57afcb3b8576af7aa4
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-06-29 15:21:48 +0000

    handle multiple settled promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,15 +4,15 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
-        let array = []
-        for (let i = 0; i < results.length; i++) {
-            const values = {
-                status: results[i].status,
-                value:
+      const array = [];
+      for (let i = 0; i < results.length; i += 1) {
+        const values = {
+          status: results[i].status,
+          value:
                     results[i].status === 'fulfilled' ? results[i].value : String(results[i].reason),
-            }
-            array.push(values);
-        }
-        return array;
+        };
+        array.push(values);
+      }
+      return array;
     });
 }

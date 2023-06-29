5c6fe122e40d3146c357edacb642d65139bebc34
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-06-29 18:24:42 +0000

    catch an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,13 @@
-export default function guardrail (mathFunction) {
-    const queue = [];
-    try {
-        const value = mathFunction();
-        queue.push(value);
-        queue.push('Guardrail was processed');
-        return queue;
-    }
-    catch (error) {
-        queue.push(error.message);
-        queue.push('Guardrail was processed');
-        return queue;
-    }
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const value = mathFunction();
+    queue.push(value);
+    queue.push('Guardrail was processed');
+    return queue;
+  } catch (error) {
+    queue.push(error.message);
+    queue.push('Guardrail was processed');
+    return queue;
+  }
 }

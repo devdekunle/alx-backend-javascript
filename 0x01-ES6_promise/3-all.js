19b35d28703d068714911b4b24fcfcd5147e6d5e
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-06-29 12:03:49 +0000

    resolve multiple promises at once

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    return new Promise(()=> {
-        Promise.all([uploadPhoto(), createUser()])
-        .then((results) => {
-            console.log(results[0].body, results[1].firstName, results[1].lastName);
-        })
-        .catch(() => {
+  return new Promise(() => {
+    Promise.all([uploadPhoto(), createUser()])
+      .then((results) => {
+        console.log(results[0].body, results[1].firstName, results[1].lastName);
+      })
+      .catch(() => {
         console.log('Signup system offline');
-        });
+      });
   });
 }

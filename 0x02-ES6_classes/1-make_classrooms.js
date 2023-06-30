b1f69cf0d768f9dad91a0025d3aa4ca154de1fcd
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-06-30 08:56:29 +0000

    make three more ClassRoom objects

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,13 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    const classRoomObjects = [];
-    const class_1 = new ClassRoom(19);
-    const class_2 = new ClassRoom(20);
-    const class_3 = new ClassRoom(34);
-    classRoomObjects.push(class_1);
-    classRoomObjects.push(class_2);
-    classRoomObjects.push(class_3);
+  const classRoomObjects = [];
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+  classRoomObjects.push(class1);
+  classRoomObjects.push(class2);
+  classRoomObjects.push(class3);
 
-    return classRoomObjects;
+  return classRoomObjects;
 }

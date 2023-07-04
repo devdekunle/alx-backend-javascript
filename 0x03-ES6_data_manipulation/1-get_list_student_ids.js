8d69246703e524f76ccab39c6e88ba079c51bfb2
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-04 14:15:34 +0000

    fix eslint error

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(array) {
-  if (Array.isArray(array) == false) {
+  if (Array.isArray(array) === false) {
     return [];
   }
   const arrayIds = array.map((object) => object.id);

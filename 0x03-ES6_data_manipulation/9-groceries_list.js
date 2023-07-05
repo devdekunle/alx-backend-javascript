7def81d2c7a94b180f29906df64a91f631d7f305
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-05 22:13:08 +0000

    create a Map object

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function () {
+  const newMap = new Map();
+  newMap.set('Apples', 10);
+  newMap.set('Tomatoes', 10);
+  newMap.set('Pasta', 1);
+  newMap.set('Rice', 1);
+  newMap.set('Banana', 5);
+
+  return newMap;
+}

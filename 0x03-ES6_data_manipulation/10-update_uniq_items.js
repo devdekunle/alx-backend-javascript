f02393b1a1399ffc7846f9edab588fb5e79c087e
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-05 23:05:43 +0000

    update clone instance

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,8 +2,6 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-
-  // const newMap = new Map(map)
   for (const [key, value] of map) {
     if (value === 1) {
       map.set(key, 100);

04b5058297b60f74157c367fe9e15cf3c30e5476
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-25 18:19:05 +0100

    refactor createServer method to ne asynchronous

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,26 +1,23 @@
-const countStudents = require('./3-read_file_async');
 const http = require('http');
-const database = process.argv[2]
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2];
 
 const hostname = 'localhost';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    if (req.url === '/') {
-        res.end('Hello Holberton School!')
-    } else if (req.url === '/students') {
-        res.end('This is the list of our students');
-        
-    } else {
-        res.end('Not found\n');
-    }
-
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/students') {
+    res.end(`This is the list of our students ${await countStudents(database)}`);
+  } else {
+    res.end('Hello Holberton School!');
+  }
 });
 
 app.listen(port, hostname, () => {
-    console.log('...');
+  console.log('...');
 });
 
 module.exports = app;

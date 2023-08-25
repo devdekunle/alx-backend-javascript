d861e0c6e5bc9a948209cd7b91e8e2735350e2bc
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-25 16:39:51 +0100

    added correct response from express server

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,8 +4,8 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.statusCode = 200;
-  res.send('Hello Holberton School');
+  res.statusCode = 200;
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
